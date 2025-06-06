51774cee9915aec41a786ba2e80adc434ab710ca
root <root@DESKTOP-L4NV8UO.localdomain>
2025-06-06 05:50:47 +0000

    Moved build, CMakeLists.txt, Jenkinsfile, README.md, src, and tests into C-_Application folder

---
+++ build/CMakeFiles/casino_game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/casino_game.dir/src/game.cpp.o"
+  "CMakeFiles/casino_game.dir/src/game.cpp.o.d"
+  "CMakeFiles/casino_game.dir/src/main.cpp.o"
+  "CMakeFiles/casino_game.dir/src/main.cpp.o.d"
+  "CMakeFiles/casino_game.dir/src/player.cpp.o"
+  "CMakeFiles/casino_game.dir/src/player.cpp.o.d"
+  "casino_game"
+  "casino_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/casino_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/test_game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_game.dir/src/game.cpp.o"
+  "CMakeFiles/test_game.dir/src/game.cpp.o.d"
+  "CMakeFiles/test_game.dir/src/player.cpp.o"
+  "CMakeFiles/test_game.dir/src/player.cpp.o.d"
+  "CMakeFiles/test_game.dir/tests/test_game.cpp.o"
+  "CMakeFiles/test_game.dir/tests/test_game.cpp.o.d"
+  "test_game"
+  "test_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
